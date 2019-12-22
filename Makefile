f920035f9e6c2f0eedb79c9d283a487d30786b68
Madelyn Eriksen <hello@madelyneriksen.com>
2019-12-22 11:10:46 -0800

    Added basic testing infrastructure.

--- Makefile
+++ Makefile
@@ -1,7 +1,18 @@
-.PHONY: build upload
+.ONESHELL:
+SHELL := /bin/bash
+.PHONY: build upload format test
 
 build:
 	python setup.py sdist bdist_wheel
 
 upload:
 	python -m twine upload dist/*
+
+format:
+	black dashml
+
+test:
+	pytest
+	mypy --strict dashml
+	black --check dashml
+	exit ${STAT}
--- dashml/__init__.py
+++ dashml/__init__.py
@@ -1,6 +1,9 @@
 import typing as t
 from functools import singledispatch, partial
 
+if t.TYPE_CHECKING:
+    from mypy_extensions import VarArg, KwArg
+
 from lxml.etree import Element
 import lxml.html as html
 from lxml.builder import E as raw_builder
@@ -13,6 +16,43 @@ __all__ = ["_", "render"]
 
 T = t.TypeVar("T")
 
+# Like `className` or `htmlFor` in React.
+RESERVED_PAIRS = [
+    ["class_name", "class"],
+    ["html_for", "for"],
+]
+
+
+Child = t.Union[Element, str]
+
+
+class Builder:
+    """HTML5-aware builder class for Python."""
+
+    def __getattr__(
+        self, attr: str
+    ) -> t.Callable[[VarArg(Child), KwArg(t.Any)], Element]:
+        """Get an attribute."""
+        return partial(self.__build, attr)
+
+    def __build(self, tag_name: str, *children: Child, **props: t.Any) -> Element:
+        """Build an element."""
+        for from_attr, to_attr in RESERVED_PAIRS:
+            swap(from_attr, to_attr, props)
+
+        safe_children = list(map(safe, children))
+
+        tag = getattr(raw_builder, tag_name)
+        return tag(*safe_children, **props)
+
+
+def render(ele: Element) -> str:
+    raw: bytes = html.tostring(ele)
+    return raw.decode("utf-8")
+
+
+_ = Builder()
+
 
 @singledispatch
 def safe(var: T) -> T:
@@ -20,8 +60,8 @@ def safe(var: T) -> T:
     return var
 
 
-@safe.register(str)
-def _(var: str) -> str:
+@safe.register
+def __safe_string(var: str) -> str:
     """Escape a string."""
     return str(markupsafe.escape(var))
 
@@ -31,38 +71,3 @@ def swap(from_attr: str, to_attr: str, values: t.Dict[str, str]) -> None:
     contained_value = values.pop(from_attr, None)
     if contained_value is not None:
         values[to_attr] = contained_value
-    return values
-
-
-# Like `className` or `htmlFor` in React.
-RESERVED_PAIRS = [
-    ["class_name", "class"],
-    ["html_for", "for"],
-]
-
-
-class Builder:
-    """HTML5-aware builder class for Python."""
-
-    def __call__(self, tag: str, *children, **props) -> Element:
-        """Direct access based on strings."""
-        return self.__build(tag, *children, **props)
-
-    def __getattr__(self, attr: str):
-        """Get an attribute."""
-        return partial(self.__build, attr)
-
-    def __build(self, tag: str, *children, **props):
-        """Build an element."""
-        for from_attr, to_attr in RESERVED_PAIRS:
-            swap(from_attr, to_attr, props)
-        children = list(map(safe, children))
-        tag = getattr(raw_builder, tag)
-        return tag(*children, **props)
-
-
-def render(ele: Element) -> str:
-    return html.tostring(ele).decode("utf-8")
-
-
-_ = Builder()
