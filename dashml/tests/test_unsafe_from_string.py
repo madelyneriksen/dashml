ffce159819a613c5d29e003c2b432965429f5c11
Madelyn Eriksen <hello@madelyneriksen.com>
2019-12-22 14:49:34 -0800

    Added a couple other test cases.

--- dashml/tests/test_unsafe_from_string.py
+++ dashml/tests/test_unsafe_from_string.py
@@ -7,6 +7,13 @@ from dashml import unsafe_from_string
 
 def test_unsafe_string_loading():
     """Test the loading of unsafe html."""
+
     ele = unsafe_from_string("<script>alert()</script>")
     assert len(ele.xpath("//script")) == 1
     assert ele.xpath("//script")[0].text == "alert()"
+
+    ele = unsafe_from_string("<p>Hello world!</p>")
+    assert ele.text == "Hello world!"
+
+    ele = unsafe_from_string('<img src="logo.png">')
+    assert ele.get("src") == "logo.png"
