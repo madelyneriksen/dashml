9db6c64813cee4711422cd870e06d8b282870c7a
Madelyn Eriksen <hello@madelyneriksen.com>
2019-12-23 06:25:07 -0800

    Moved html-interfacing components to module.

--- dashml/core.py
+++ dashml/core.py
@@ -1,39 +1,13 @@
 import typing as t
 from functools import partial
 
-import lxml.html as html
-
 from dashml.builder import Builder
 from dashml.clean import safe_children, safe_props
 from dashml.types import Child, Prop, BuilderCallable, Element
+from dashml.html import render, unsafe_from_string
 
 
 __all__ = ["_", "render", "unsafe_from_string"]
 
 
 _ = Builder()
-
-
-def render(ele: Element) -> str:
-    """Render an Element to a string.
-
-    Arguments:
-        ele (Element): The element to render.
-    Returns:
-        (str) Rendered utf-8 string of the element.
-    """
-    return html.tostring(ele).decode("utf-8")
-
-
-def unsafe_from_string(unsafe_string: str) -> Element:
-    """UNSAFE: Create an element from a string, skipping escaping.
-
-    HTML will _not_ be escaped by this function, so using it with an untrusted
-    string is a security vulnerability and could allow XSS attacks!
-
-    Arguments:
-        unsafe_string (str): A string to convert to an element.
-    Returns:
-        (Element) The converted element
-    """
-    return html.fromstring(unsafe_string)
---
+++ dashml/html.py
@@ -0,0 +1,42 @@
+"""Functions for exporting DashML to (or importing from) raw HTML.
+
+The most notable function here is `render()`, which takes a DashML element
+as an argument and returns an HTML representation of it.
+
+>>> from dashml import _, render
+>>> render(_.p("Hello world!"))
+
+Additionally, the method `unsafe_from_string` exists here to transform
+trusted HTML strings into DashML/lxml Element instances. Use this great
+unsafe power responsibly!
+"""
+
+
+import lxml.html as html
+
+from dashml.types import Element
+
+
+def render(ele: Element) -> str:
+    """Render an Element to a string.
+
+    Arguments:
+        ele (Element): The element to render.
+    Returns:
+        (str) Rendered utf-8 string of the element.
+    """
+    return html.tostring(ele).decode("utf-8")
+
+
+def unsafe_from_string(unsafe_string: str) -> Element:
+    """UNSAFE: Create an element from a string, skipping escaping.
+
+    HTML will _not_ be escaped by this function, so using it with an untrusted
+    string is a security vulnerability and could allow XSS attacks!
+
+    Arguments:
+        unsafe_string (str): A string to convert to an element.
+    Returns:
+        (Element) The converted element
+    """
+    return html.fromstring(unsafe_string)
